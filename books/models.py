c328a599502432801c908719dd22271158790c22
Sangmo Lama <sangmolama29@gmail.com>
2026-03-13 19:12:11 +0545

    created book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=20)
+    published_date = models.DateField(null=True, blank = True)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
