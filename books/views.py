ed7d617b6469d464bb93948e64c04026c82c402b
Sangmo Lama <sangmolama29@gmail.com>
2026-03-13 19:49:26 +0545

    updated form and views

--- books/views.py
+++ books/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import BookForm
 from .models import Book
 
 
 def book_list(request):
 	books = Book.objects.order_by("title")
 	return render(request, "books/book_list.html", {"books": books})
+
+
+def book_create(request):
+	if request.method == "POST":
+		form = BookForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("books:book_list")
+	else:
+		form = BookForm()
+
+	return render(request, "books/book_form.html", {"form": form})
