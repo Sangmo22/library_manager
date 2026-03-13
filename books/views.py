41e78dc9ba1b8621c9fa2c4b2378bbcababb4d2d
Sangmo Lama <sangmolama29@gmail.com>
2026-03-13 20:59:36 +0545

    added book edit

--- books/views.py
+++ books/views.py
@@ -19,3 +19,16 @@ def book_create(request):
 		form = BookForm()
 
 	return render(request, "books/book_form.html", {"form": form})
+
+
+def book_edit(request, pk):
+	book = Book.objects.get(pk=pk)
+	if request.method == "POST":
+		form = BookForm(request.POST, instance=book)
+		if form.is_valid():
+			form.save()
+			return redirect("books:book_list")
+	else:
+		form = BookForm(instance=book)
+
+	return render(request, "books/book_form.html", {"form": form})
