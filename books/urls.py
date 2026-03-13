19215c00c6ba1b513453cbe37a5abe328c6f95e6
Sangmo Lama <sangmolama29@gmail.com>
2026-03-13 19:43:14 +0545

    added admin site,in  urls, views, book_list

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "books"
+
+urlpatterns = [
+    path("", views.book_list, name="book_list"),
+    path("add/", views.book_create, name="book_create"),
+]
