8b018f6318e40dfdd8e2651f9058f9e285558d9b
Sangmo Lama <sangmolama29@gmail.com>
2026-03-13 21:45:02 +0545

    added book detail

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,7 @@ app_name = "books"
 urlpatterns = [
     path("", views.book_list, name="book_list"),
     path("add/", views.book_create, name="book_create"),
+    path("book/<int:pk>/", views.book_detail, name="book_detail"),
     path("edit/<int:pk>/", views.book_edit, name="book_edit"),
     path("delete/<int:pk>/", views.book_delete, name="book_delete"),
 ]
