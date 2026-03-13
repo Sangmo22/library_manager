47c1e5082f876e185684c5547b17e2f78350700e
Sangmo Lama <sangmolama29@gmail.com>
2026-03-13 21:05:42 +0545

    Added edit button and book edit path to urls

--- books/urls.py
+++ books/urls.py
@@ -7,4 +7,5 @@ app_name = "books"
 urlpatterns = [
     path("", views.book_list, name="book_list"),
     path("add/", views.book_create, name="book_create"),
+    path("edit/<int:pk>/", views.book_edit, name="book_edit"),
 ]
